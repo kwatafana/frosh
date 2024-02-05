be2493a34a0617ba2c2f28909c810e9adf904ddd
Jackson G. Kaindume <cy6erlion@protonmail.com>
2024-02-05 12:36:53 +0200

    [crates/gender]: use Gender type from the kommon crate

--- crates/data/src/student.rs
+++ crates/data/src/student.rs
@@ -1,31 +1,35 @@
 //! Frosh Student Data Type
 
-use crate::gender::Gender;
 use chrono::{DateTime, Utc};
+use kommon::Gender;
+use serde::{Deserialize, Serialize};
 
 /// Student data type
-pub struct Student{
+#[derive(PartialEq, Debug, Serialize, Deserialize)]
+pub struct Student {
     /// Students account username
     pub username: String,
     /// Students first name
     pub firstname: String,
     /// Students middle names
-    pub middlenames: String,
+    pub middlenames: Option<String>,
     /// Students last name
     pub lastname: String,
     /// Students email address
     pub email: String,
     /// Short bio of student
-    pub bio: String,
+    pub bio: Option<String>,
     /// Courses, it is u32 because it represents the course ID in
     /// the courses database
     pub courses: Vec<u32>,
     /// Student's number
-    pub student_number: String,
+    pub student_number: Option<String>,
     /// Student national ID number
-    pub national_id: String,
+    pub national_id: Option<String>,
     /// Students mobile phone number
-    pub mobile: String,
+    pub mobile: Option<String>,
+    #[serde(deserialize_with = "Gender::deserialize")]
+    #[serde(serialize_with = "Gender::serialize")]
     /// Students gender
     pub gender: Gender,
     /// Last time the student logged into Frosh
